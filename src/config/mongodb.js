a7dd8839c53b449d806dcfa519d8e824c2be7c61
Samia Ahmed Chowdhury <samiachowdhury546@gmail.com>
2024-01-22 23:19:53 +0600

    L

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
+    console.log("MONGODB_URI:",typeof( process.env.MONGODB_URI));
+await mongoose.connect(process.env.MONGODB_URI,{
+   useNewUrlParser: true 
+});
+
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB.");
   } catch (error) {
